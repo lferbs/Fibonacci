59c95b7a1e38ff40f3a352d9861f84aec2b41be9
Fernando Gutierrez <31115859+LFernandoGO@users.noreply.github.com>
2020-09-29 19:08:57 -0500

    Add files via upload
    Codigo fibonacci realizado en clase

---
+++ fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+   int fibonacci,n,cont;
+
+    int val1 = 0;
+
+    int val2 = 1;
+
+   printf("Ingresa el numero que quieres obtener de la serie de fibonacci");
+
+   scanf("%d",&n);
+
+   for(cont=0;cont<n;cont++){
+
+             fibonacci = val1 + val2;
+
+             printf("%d\n",fibonacci);
+
+             val1 = val2;
+
+             val2 = fibonacci;
+
+   }
+
+   return 0;
+
+}
